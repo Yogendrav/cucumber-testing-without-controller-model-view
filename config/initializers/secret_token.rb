365b2a10d2b27fa29d5c5ac66134c33510ea73e2
ntf <ntf@linux.local>
2012-12-14 19:13:17 +0530

    cucumber-testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CucumberTesting::Application.config.secret_token = '4f879427b814c011b92bc21e7bfe61606849994d5e04365baed18efd9b365e73a3d9a70f77bd4678b0dea64636eb6dd78fef7c9f9d21111ae0be1360e6b684c3'
